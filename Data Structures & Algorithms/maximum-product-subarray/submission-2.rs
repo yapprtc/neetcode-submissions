dc4a318932fa463b546f0bf9a9132589b772dd76
yapprtc <46794144+yapprtc@users.noreply.github.com>
2026-04-29 20:52:04 -0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        assert!(!nums.is_empty());
+        let (mut min_local, mut max_local, mut global) = (nums[0], nums[0], nums[0]);
+        for j in 1..nums.len() {
+            let prev_max_local = max_local;
+            max_local = std::cmp::max(std::cmp::max(max_local * nums[j], min_local * nums[j]), nums[j]);
+            min_local = std::cmp::min(std::cmp::min(min_local * nums[j], prev_max_local*nums[j]), nums[j]);
+            global = std::cmp::max(global, max_local);
+        }
+        global
+    }
+}
